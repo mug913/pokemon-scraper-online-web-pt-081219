98ecdde85f4b8a09bc9ca37139379f989d0b4f43
Brad Alexander <brad.alexander913@gmail.com>
2019-11-06 01:28:27 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,13 @@
 class Pokemon
+
+  attr_accessor :name, :type, :db
+  attr_reader :id
+
+  def initialize(name,type,db,id=nil)
+    @name = name
+    @type = type
+    @id = id
+    @db = db
+
+  end
 end
