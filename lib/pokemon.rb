9f9acdefb61ef47b2b8fa8cb83178c732ae7d285
Brad Alexander <brad.alexander913@gmail.com>
2019-11-06 01:33:54 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -7,6 +7,15 @@ class Pokemon
     @name = name
     @type = type
     @id = id
+    @db = db
+  end
 
+  def.save
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type)
+      VALUES (?, ?)
+    SQL
+    DB[:conn].execute(sql, self.name, self.type, self.db)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
 end
